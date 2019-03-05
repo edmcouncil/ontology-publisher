81b7b7730161caf483f90f6e2ee886024708627d
Scott Came <scott.came@data.world>
2019-03-05 13:57:15 -0800

    Refactorings to add cli params, modularize source and sink, PEP style guide, etc.

--- publisher/lib/subset.py
+++ publisher/lib/subset.py
@@ -23,100 +23,152 @@
 """
 
 """
-import pdb
 import sys
-import string
-import sys
+import argparse
+from os import walk, path, makedirs
+from abc import ABC, abstractmethod
+
 import rdflib
-from itertools import chain
-from rdflib.namespace import SKOS, Namespace, NamespaceManager, RDF, XSD, OWL, RDFS
-from rdflib import URIRef, BNode, Literal
-import re
-from os import scandir, walk, path, makedirs
+from rdflib.namespace import RDF, OWL
+
+ENCODING = 'utf-8'
 
 class TBCGraph(rdflib.Graph):
-    def serialize (self, **kwargs):
-        onturi=list(self.triples((None, RDF.type, OWL.Ontology)))[0][0]
-        dest=kwargs["destination"]
-        print ("writing to "+dest)
-        relpath="/".join(dest.split("/")[0:-1])
-        print ("relpath="+relpath)
-        if (relpath!=''):
-            makedirs(relpath, exist_ok=True)
-        if (kwargs["format"]=="ttl"):
-            px="# baseURI: %s\n" % (str(onturi))
-        else:
-            px=""
-        super().serialize(**kwargs)
-        with open(dest, 'r', encoding="utf-8",) as original: data = original.read()
-        with open(dest, 'w', encoding="utf-8",) as modified: modified.write(px + data)
+    
+    def serialize(self, subsetSink, fmt, verbose):
+        
+        onturi = list(self.triples((None, RDF.type, OWL.Ontology)))[0][0]
+        
+        data = super().serialize(format=fmt, encoding=ENCODING)
+        data = str(data, ENCODING)
+        
+        subsetSink.saveSubset(data, onturi, fmt, verbose)
+        
 
+class OntologySource(ABC):
+    
+    @abstractmethod
+    def getGraphList(self):
+        pass
+    
+class FileOntologySource(OntologySource):
+    
+    def __init__(self, directory):
+        self.directory = directory
+    
+    def getGraphList(self):
+        return [TBCGraph().parse(path.join(root, name)) for root, dirs, files in walk(self.directory) if root != self.directory for name in files
+            if name.endswith(".rdf") and
+            "etc" not in root and
+            "git" not in root and
+            "About" not in name and
+            "All" not in name
+            and "Metadata" not in name]
+        
+class SubsetSink(ABC):
+    
+    @abstractmethod
+    def saveSubset(self, subsetData, ontologyURI, fmt, verbose):
+        pass
+    
+class FileSubsetSink(SubsetSink):
+    
+    def __init__(self, destination):
+        self.destination = destination
+    
+    def saveSubset(self, subsetData, ontologyURI, fmt, verbose):
+        
+        outStream = sys.stdout
+        
+        if self.destination is not None:
+            if (verbose): print("Writing subset to: " + self.destination)
+            relpath = "/".join(self.destination.split("/")[0:-1])
+            if (relpath != ''):
+                makedirs(relpath, exist_ok=True)
+            outStream = open(self.destination, 'w', encoding=ENCODING)
+        
+        px = ''
+        if (fmt == 'turtle'):
+            # todo: is this comment necessary?
+            px = "# baseURI: %s\n" % (str(ontologyURI))
+        
+        outStream.write(px + subsetData)
 
 class Factor():
-    def __init__ (self, directory, base):
-        self.base=base
-        ps=[path.join(root, name) for root, dirs, files in walk(directory) if root!=directory for name in files
-            if name.endswith(".rdf") and
-            "etc" not in root and
-            "git" not in root and
-            "About" not in name and
-            "All" not in name
-            and "Metadata" not in name]
+    
+    def __init__(self, ontologySource, base, verbose, fmt):
+        
+        self.base = base
+        
+        gs = ontologySource.getGraphList()
 
-#        print (ps)
-
-        gs=[TBCGraph().parse(path.join(root, name)) for root, dirs, files in walk(directory) if root!=directory for name in files
-            if name.endswith(".rdf") and
-            "etc" not in root and
-            "git" not in root and
-            "About" not in name and
-            "All" not in name
-            and "Metadata" not in name]
-
-        self.out=TBCGraph()
-        self.all=TBCGraph()
+        self.out = TBCGraph()
+        self.all = TBCGraph()
+        
         for g in gs:
             for n in g.namespace_manager.namespaces():
-                self.out.namespace_manager.bind (n[0], n[1])
-                self.all.namespace_manager.bind (n[0], n[1])
+                self.out.namespace_manager.bind(n[0], n[1])
+                self.all.namespace_manager.bind(n[0], n[1])
             for t in g.triples((None, None, None)):
                 self.all.add(t)
-        print (str(len(list(self.all.triples ((None, None, None)))))+" triples in all")
-        self.done=[]
+                
+        if (verbose): print('Found ' + str(len(list(self.all.triples ((None, None, None))))) + " triples in source ontology")
+        
+        self.done = []
+        self.verbose = verbose
+        self.fmt = fmt
 
-    def save (self):
-        self.all.serialize(destination="SAVE/ONTS/full.rdf", encoding="utf-8", format='xml')
-        self.all.serialize(destination="SAVE/ONTS/full.ttl", encoding="utf-8", format='turtle')
+    def save(self, subsetSink):
+        self.all.serialize(subsetSink=subsetSink, fmt=self.fmt, verbose=self.verbose)
 
-    def prime (self, seed):
-        with open(seed, 'r', encoding="utf-8",) as original:
-            s=original.readline()
-            while (s!=""):
+    def prime(self, seed):
+        if (self.verbose): print('Reading subset requirements from seeds file at: ' + seed)
+        with open(seed, 'r', encoding=ENCODING,) as original:
+            s = original.readline()
+            while (s != ""):
                 if (not s.startswith("#")):
-                    self.copy(URIRef(s.strip()), "")
-                s=original.readline()
+                    self.copy(rdflib.URIRef(s.strip()), "")
+                s = original.readline()
+        return self
 
-    def copy (self, concept, level):
+    def copy(self, concept, level):
         if (concept not in self.done):
-#            print (level+"copying "+str(concept))
-            self.done=self.done+[concept]
+            self.done = self.done + [concept]
             for t in self.all.triples((concept, None, None)):
                 self.out.add(t)
-                self.copy(t[1], level+" ")
-                self.copy(t[2], level+" ")
+                self.copy(t[1], level + " ")
+                self.copy(t[2], level + " ")
             for t in self.all.triples((None, OWL.inverseOf, concept)):
                 self.out.add(t)
-                self.copy(t[0], level+" ")
+                self.copy(t[0], level + " ")
 
-    def dodump(self):
-        self.out.add ((URIRef(self.base), RDF.type, OWL.Ontology))
-        uchop=re.sub(r'/$','',self.base)
-        self.out.serialize(destination="TEST/"+uchop.split("/")[-1]+".ttl", encoding="utf-8", format='turtle')
+    def writeSubset(self, subsetSink):
+        self.out.add((rdflib.URIRef(self.base), RDF.type, OWL.Ontology))
+        self.out.serialize(subsetSink=subsetSink, fmt=self.fmt, verbose=self.verbose)
 
+def parseArgs():
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument('ontology', help='Root directory of the ontology to subset')
+    argParser.add_argument('base', help='Base URI of subset')
+    argParser.add_argument('-f', '--format', help='Subset format (default is ttl/turtle)', default='turtle')
+    argParser.add_argument('-s', '--seeds', help='Seeds file; full subset will result if not specified')
+    argParser.add_argument('-d', '--destination', help='File into which subset will be written (in turtle format); stdout if not specified')
+    argParser.add_argument('-v', '--verbose', dest='verbose', action='store_true', default=False, help='Print dianostic/progress info')
+    return argParser.parse_args()
 
-
-f=Factor("C:\\Users\\Dean\\Documents\\fibo", "https://spec.edmcouncil.org/fibo/ontology/")
-#f=Factor("C:\\Users\\Dean\\Dropbox\\Dean\\WorkingOntologist\\FIBO\\factor\\SAVE", "https://spec.edmcouncil.org/fibo/ontology/")
-# f.save()
-f.prime("seeds")
-f.dodump()
+if __name__ == '__main__':
+    
+    args = parseArgs()
+    
+    destination = args.destination
+    seeds = args.seeds
+    
+    ontologySource = FileOntologySource(args.ontology)
+    destination = FileSubsetSink(destination)
+    
+    f = Factor(ontologySource, args.base, args.verbose, args.format)
+    
+    if seeds is None:
+        f.save(destination)
+    else:
+        f.prime(seeds).writeSubset(destination)
