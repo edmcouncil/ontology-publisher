388af83e11a74513df7b7630439a4eaa5ed9f81a
Scott Came <scottcame10@gmail.com>
2019-03-13 11:28:18 -0700

    Added pluggable logging capability (needed for Flask logging)

--- publisher/lib/subset.py
+++ publisher/lib/subset.py
@@ -37,31 +37,31 @@ ENCODING = 'utf-8'
 FIBO_FILE_IGNORE_REGEX = 'About|All|Metadata|catalog-'
 
 class TBCGraph(rdflib.Graph):
-    
-    def serialize(self, subsetSink, fmt, verbose):
-        
+
+    def serialize(self, subsetSink, fmt, verbose, log):
+
         onturi = list(self.triples((None, RDF.type, OWL.Ontology)))[0][0]
-        
+
         data = super().serialize(format=fmt, encoding=ENCODING)
         data = str(data, ENCODING)
-        
-        subsetSink.saveSubset(data, onturi, fmt, verbose)
+
+        subsetSink.saveSubset(data, onturi, fmt, verbose, log)
 
 class OntologySource(ABC):
-    
+
     @abstractmethod
     def getGraphList(self, verbose):
         pass
-    
+
 class FileOntologySource(OntologySource):
-    
+
     def __init__(self, sourceFile):
         self.sourceFile = sourceFile
-    
+
     def getGraphList(self, verbose):
-        
+
         ret = []
-        
+
         if isdir(self.sourceFile):
             for root, dirs, files in walk(self.sourceFile):
                 if root != self.sourceFile and not re.search('etc|git', root):
@@ -74,9 +74,9 @@ class FileOntologySource(OntologySource):
             g = self.parseFile(self.sourceFile)
             if g:
                 ret.append(g)
-                            
+
         return ret
-    
+
     def parseFile(self, fileName):
         ret = None
         name = basename(fileName)
@@ -88,84 +88,85 @@ class FileOntologySource(OntologySource):
                     fe = 'xml'
                 ret = TBCGraph().parse(fileName, format=fe)
         return ret
-        
-        
+
+
 class SubsetSink(ABC):
-    
+
     @abstractmethod
-    def saveSubset(self, subsetData, ontologyURI, fmt, verbose):
+    def saveSubset(self, subsetData, ontologyURI, fmt, verbose, log):
         pass
-    
+
 class FileSubsetSink(SubsetSink):
-    
+
     def __init__(self, destination):
         self.destination = destination
-    
-    def saveSubset(self, subsetData, ontologyURI, fmt, verbose):
-        
+
+    def saveSubset(self, subsetData, ontologyURI, fmt, verbose, log):
+
         outStream = sys.stdout
-        
+
         if self.destination is not None:
-            if (verbose): print("Writing subset to: " + self.destination)
+            if (verbose): log("Writing subset to: " + self.destination)
             relpath = "/".join(self.destination.split("/")[0:-1])
             if (relpath != ''):
                 makedirs(relpath, exist_ok=True)
             outStream = open(self.destination, 'w', encoding=ENCODING)
-        
+
         px = ''
         if (fmt == 'turtle'):
             # todo: is this comment necessary?
             px = "# baseURI: %s\n" % (str(ontologyURI))
-        
+
         outStream.write(px + subsetData)
 
 class Factor():
-    
-    def __init__(self, ontologySource, base, verbose):
-        
+
+    def __init__(self, ontologySource, base, verbose, log=lambda msg: print(msg)):
+
         self.base = base
-        
+
         gs = ontologySource.getGraphList(verbose)
 
         self.out = TBCGraph()
         self.all = TBCGraph()
-        
+
         for g in gs:
             for n in g.namespace_manager.namespaces():
                 self.out.namespace_manager.bind(n[0], n[1])
                 self.all.namespace_manager.bind(n[0], n[1])
             for t in g.triples((None, None, None)):
                 self.all.add(t)
-                
-        if (verbose): print('Found ' + str(len(list(self.all.triples ((None, None, None))))) + " triples in source ontology")
-        
+
+        if (verbose): log('Found ' + str(len(list(self.all.triples ((None, None, None))))) + " triples in source ontology")
+
         self.done = []
         self.verbose = verbose
+        self.log = log
 
     def save(self, subsetSink, fmt):
-        self.all.serialize(subsetSink=subsetSink, fmt=fmt, verbose=self.verbose)
+        self.all.serialize(subsetSink=subsetSink, fmt=fmt, verbose=self.verbose, log=self.log)
 
     def prime(self, seedFile=None, seedList=None):
-        
+
         if seedFile is not None:
             seedList = []
-            if (self.verbose): print('Reading subset requirements from seeds file at: ' + seedFile)
+            if (self.verbose): self.log('Reading subset requirements from seeds file at: ' + seedFile)
             with open(seedFile, 'r', encoding=ENCODING,) as original:
                 s = original.readline()
                 while (s != ""):
                     seedList.append(s)
                     s = original.readline()
-                    
+
         filteredSeedList = []
         for seed in seedList:
             if (not seed.startswith("#")):
                 filteredSeedList.append(seed)
-        
-        if (self.verbose): print('Subsetting ontology for ' + str(len(filteredSeedList)) + ' seeds')
-        
+
+        if (self.verbose): self.log('Subsetting ontology for ' + str(len(filteredSeedList)) + ' seeds')
+
         for seed in filteredSeedList:
-            self.copy(rdflib.URIRef(seed.strip()), "")     
-                  
+            self.copy(rdflib.URIRef(seed.strip()), "")
+
         return self
 
     def copy(self, concept, level):
@@ -181,7 +182,7 @@ class Factor():
 
     def writeSubset(self, subsetSink, fmt):
         self.out.add((rdflib.URIRef(self.base), RDF.type, OWL.Ontology))
-        self.out.serialize(subsetSink=subsetSink, fmt=fmt, verbose=self.verbose)
+        self.out.serialize(subsetSink=subsetSink, fmt=fmt, verbose=self.verbose, log=self.log)
 
 def parseArgs():
     argParser = argparse.ArgumentParser()
@@ -194,17 +195,17 @@ def parseArgs():
     return argParser.parse_args()
 
 if __name__ == '__main__':
-    
+
     args = parseArgs()
-    
+
     destination = args.destination
     seeds = args.seeds
-    
+
     ontologySource = FileOntologySource(args.ontology)
     destination = FileSubsetSink(destination)
-    
+
     f = Factor(ontologySource, args.base, args.verbose)
-    
+
     if seeds is None:
         f.save(destination, args.format)
     else:
