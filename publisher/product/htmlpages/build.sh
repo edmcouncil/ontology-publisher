71668393f24666ee460ab5c1d393d846ab2db210
Przemysław Grądzki <przemyslaw.gradzki@makolab.com>
2019-09-27 18:57:21 +0200

    htmlpages.log improvement

--- publisher/product/htmlpages/build.sh
+++ publisher/product/htmlpages/build.sh
@@ -22,25 +22,25 @@ function publishProductHTMLPages () {
   FVUE_SRC_PATH="/etc/fibo-vue"
   FVUE_PATH="${TMPDIR}/fibo-vue"
 
+  touch "${tag_root:?}/htmlpages.log"
+
   # return success if "fibo-vue" doesn't exist
   test -d "${source_family_root:?}${FVUE_SRC_PATH:?}" || return 0
 
   logItem "Prepare" "$(logFileName "${FVUE_PATH:?}")"
-  rm -rf "${FVUE_PATH:?}" &>/dev/null || return $?
+  rm -rvf "${FVUE_PATH:?}" >> "${tag_root:?}/htmlpages.log" 2>&1 || return $?
   logItem "Copy" "$(logFileName "${source_family_root:?}${FVUE_SRC_PATH:?} -> ${FVUE_PATH:?}")"
-  cp -a "${source_family_root:?}${FVUE_SRC_PATH:?}" "${FVUE_PATH:?}"
+  cp -av "${source_family_root:?}${FVUE_SRC_PATH:?}" "${FVUE_PATH:?}" >> "${tag_root:?}/htmlpages.log" 2>&1 || return $?
 
   pushd "${FVUE_PATH:?}"
   logItem "npm --unsafe-perm install" "$(logFileName "${FVUE_PATH:?}")"
-  env HOME="${TMPDIR:?}" npm --unsafe-perm install 2>&1 || return $?
+  env HOME="${TMPDIR:?}" npm --unsafe-perm install >> "${tag_root:?}/htmlpages.log" 2>&1 || return $?
   logItem "npm run build" "$(logFileName "${FVUE_PATH:?}")"
-  env HOME="${TMPDIR:?}" npm --unsafe-perm run build 2>&1 || return $?
+  env HOME="${TMPDIR:?}" npm --unsafe-perm run build >> "${tag_root:?}/htmlpages.log" 2>&1 || return $?
   popd
 
   logItem "copy" "$(logFileName "${FVUE_PATH:?}/dist/${product_branch_tag:?} -> ${tag_root}")"
-  rm -rf "${tag_root:?}" && cp -a "${FVUE_PATH:?}/dist/${product_branch_tag:?}" "${tag_root:?}" 2>&1 || return $?
-
-  touch "${tag_root:?}/htmlpages.log"
+  rm -rf "${tag_root:?}" && cp -av "${FVUE_PATH:?}/dist/${product_branch_tag:?}" "${tag_root:?}" >> "${tag_root:?}/htmlpages.log" 2>&1 || return $?
 
   return $?
 }
