c5b7a77983141f9eda0e9e1c1229d119d388c66e
Sam <samuellstouffer@gmail.com>
2020-09-15 15:02:23 -0400

    update dsa-docker-run

--- dsa-docker-run.sh
+++ dsa-docker-run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-export HOME=${$HOME:-/home/sam/workspace}
+PARENT_DIR=$(cd .. && pwd)
+export HOME=${$HOME:-$PARENT_DIR}
 export ONTPUB_FAMILY=${ONTPUB_FAMILY:-DynamicSpectrumAccess}
 export ONTPUB_EXEC="./publish.sh hygiene"
 export ONTPUB_SUBDIR="/ontologies"
 export ONTPUB_EXCLUDED="/archived"
+export HYGIENE_NAMESPACE_REGEX="twc"
+export RUN_OPT=${RUN_OPT:-"--run"}
 
-# ./docker-run.sh --dev --build --shell
-./docker-run.sh --dev --build --run
+./docker-run.sh --dev --build ${RUN_OPT}
