9f8164679b4c6734849663a9a6a76595dbe99dad
Pawel Garbacz <pawel.garbacz@makolab.com>
2021-02-25 13:42:32 +0000

    applied https://jira.makolab.net/secure/attachment/206763/glossary-build.patch
    Signed-off-by: Pawel Garbacz <pawel.garbacz@makolab.com>

--- publisher/product/glossary/build.sh
+++ publisher/product/glossary/build.sh
@@ -20,7 +20,6 @@ function publishProductGlossary() {
   export glossary_product_tag_root="${tag_root:?}"
   export glossary_product_tag_root_url="${tag_root_url:?}"
 
-  
   publishProductGlossaryContent || return $?
 
   return 0
@@ -31,65 +30,66 @@ function publishProductGlossary() {
 #
 function publishProductGlossaryContent() {
 
-	logRule "Publishing the content files of the glossary product"
+  logRule "Publishing the content files of the glossary product"
 
-	require ontology_product_tag_root || return $?
-	require glossary_product_tag_root || return $?
-	
-	echo "glossary_product_tag_root_url=${glossary_product_tag_root_url}"
+  require ontology_product_tag_root || return $?
+  require glossary_product_tag_root || return $?
 
-	export glossary_script_dir="${SCRIPT_DIR:?}/product/glossary"
+  echo "glossary_product_tag_root_url=${glossary_product_tag_root_url}"
 
-	if [ ! -d "${glossary_script_dir}" ] ; then
-		error "Could not find ${glossary_script_dir}"
-		return 1
-	fi
+  export glossary_script_dir="${SCRIPT_DIR:?}/product/glossary"
 
-	logRule "Collecting DEV and PROD ontologies"
-	
-	${PYTHON3} ${SCRIPT_DIR}/lib/fibos_collector.py --input_folder /input/fibo --output_dev ${TMPDIR}/dev.rdf --output_prod ${TMPDIR}/prod.rdf 
+  if [ ! -d "${glossary_script_dir}" ] ; then
+    error "Could not find ${glossary_script_dir}"
+    return 1
+  fi
+
+  local glossaryBaseName="${glossary_product_tag_root}/glossary"
+
+  logRule "Collecting DEV and PROD ontologies"
+
+  ${PYTHON3} ${SCRIPT_DIR}/lib/fibos_collector.py --input_folder "${ontology_product_tag_root}" --output_dev ${TMPDIR}/dev.rdf --output_prod ${TMPDIR}/prod.rdf
+
+  if [ ${PIPESTATUS[0]} -ne 0 ] ; then
+    error "Could not collect FIBO ontologies"
+    return 1
+  fi
   
-	if [ ${PIPESTATUS[0]} -ne 0 ] ; then
-		error "Could not collect FIBO ontologies"
-		return 1
-	fi
+  logRule "Creating data dictionaries for DEV and PROD"
   
-	logRule "Creating data dictionaries for DEV and PROD"
-  
-	#
-	# Set the memory for ARQ
-	#
-	JVM_ARGS="--add-opens java.base/java.lang=ALL-UNNAMED"
-	JVM_ARGS="${JVM_ARGS} -Dxxx=arq"
-	JVM_ARGS="${JVM_ARGS} -Xms2g"
-	JVM_ARGS="${JVM_ARGS} -Xmx4g"
-	JVM_ARGS="${JVM_ARGS} -Dfile.encoding=UTF-8"
-	JVM_ARGS="${JVM_ARGS} -Djava.io.tmpdir=\"${TMPDIR}\""
-	export JVM_ARGS
-	logVar JVM_ARGS
+  #
+  # Set the memory for ARQ
+  #
+  JVM_ARGS="--add-opens java.base/java.lang=ALL-UNNAMED"
+  JVM_ARGS="${JVM_ARGS} -Dxxx=arq"
+  JVM_ARGS="${JVM_ARGS} -Xms2g"
+  JVM_ARGS="${JVM_ARGS} -Xmx4g"
+  JVM_ARGS="${JVM_ARGS} -Dfile.encoding=UTF-8"
+  JVM_ARGS="${JVM_ARGS} -Djava.io.tmpdir=\"${TMPDIR}\""
+  export JVM_ARGS
+  logVar JVM_ARGS
 	
-	${JENA_ARQ} --data ${TMPDIR}/dev.rdf --query ${SCRIPT_DIR}/product/glossary/data_dictionary.sparql --results=CSV > ${TMPDIR}/dev_proto_data_dictionary.csv
-	${JENA_ARQ} --data ${TMPDIR}/prod.rdf --query ${SCRIPT_DIR}/product/glossary/data_dictionary.sparql --results=CSV > ${TMPDIR}/prod_proto_data_dictionary.csv
+  ${JENA_ARQ} --data ${TMPDIR}/dev.rdf --query ${SCRIPT_DIR}/product/glossary/data_dictionary.sparql --results=CSV > ${TMPDIR}/dev_proto_data_dictionary.csv
+  ${JENA_ARQ} --data ${TMPDIR}/prod.rdf --query ${SCRIPT_DIR}/product/glossary/data_dictionary.sparql --results=CSV > ${TMPDIR}/prod_proto_data_dictionary.csv
         
-	logRule "Extending data dictionaries with generated definitions"
+  logRule "Extending data dictionaries with generated definitions"
 		
-	${PYTHON3} ${SCRIPT_DIR}/lib/dictionary_maker_no_sparql.py --input ${TMPDIR}/dev_proto_data_dictionary.csv --output ${glossary_product_tag_root}/glossary-dev.csv
-  	${PYTHON3} ${SCRIPT_DIR}/lib/dictionary_maker_no_sparql.py --input ${TMPDIR}/prod_proto_data_dictionary.csv --output ${glossary_product_tag_root}/glossary-prod.csv
+  ${PYTHON3} ${SCRIPT_DIR}/lib/dictionary_maker_no_sparql.py --input ${TMPDIR}/dev_proto_data_dictionary.csv --output "${glossaryBaseName}-dev.csv"
+  ${PYTHON3} ${SCRIPT_DIR}/lib/dictionary_maker_no_sparql.py --input ${TMPDIR}/prod_proto_data_dictionary.csv --output "${glossaryBaseName}-prod.csv"
   
-	logRule "Writing from csv files to xlsx files"
+  logRule "Writing from csv files to xlsx files"
 
-	touch "${glossary_product_tag_root}/glossary.log"	
+  touch "${glossary_product_tag_root}/glossary.log"
+  
+  ${PYTHON3} ${SCRIPT_DIR}/lib/csv-to-xlsx.py \
+    "${glossaryBaseName}-prod.csv" \
+    "${glossaryBaseName}-prod.xlsx" \
+    "${glossary_script_dir}/csvconfig"
 
-	${PYTHON3} ${SCRIPT_DIR}/lib/csv-to-xlsx.py \	
-	   "${glossary_product_tag_root}/glossary-prod.csv" \	
-	   "${glossary_product_tag_root}/glossary-prod.xlsx" \	
-	   "${glossary_script_dir}/csvconfig"	
-
-
-	${PYTHON3} ${SCRIPT_DIR}/lib/csv-to-xlsx.py \	
-	   "${glossary_product_tag_root}/glossary-dev.csv" \	
-	   "${glossary_product_tag_root}/glossary-dev.xlsx" \	
-	   "${glossary_script_dir}/csvconfig"
-
+  ${PYTHON3} ${SCRIPT_DIR}/lib/csv-to-xlsx.py \
+    "${glossaryBaseName}-dev.csv" \
+    "${glossaryBaseName}-dev.xlsx" \
+    "${glossary_script_dir}/csvconfig"
+  
   return 0
 }
