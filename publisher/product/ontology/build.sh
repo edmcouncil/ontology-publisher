1c8a1f69914c56754eada279a655505277315b57
mereolog <pawel.garbacz@makolab.com>
2021-08-05 13:00:12 +0200

    prod ontology product pruned - no lcc data
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

--- publisher/product/ontology/build.sh
+++ publisher/product/ontology/build.sh
@@ -659,15 +659,8 @@ __HERE__
   "${JENA_ARQ}" \
     $(grep -r 'utl-av[:;.]Release' "${source_family_root}" | sed 's/:.*$//;s/^/--data=/' | grep -F ".rdf") \
     --query=/publisher/lib/noimport_noontology.sparql \
-    --results=TTL > ${TMPDIR}/pre_prod.fibo-quickstart.ttl
+    --results=TTL > ${TMPDIR}/prod.fibo-quickstart.ttl
 	
-
-  ${JENA_ARQ} \
-    --data ${TMPDIR}/external.ttl \
-    --data ${TMPDIR}/pre_prod.fibo-quickstart.ttl \
-    --query=/publisher/lib/echo.sparql \
-    --results=TTL > ${tag_root}/prod.fibo-quickstart.ttl
-
   ${JENA_ARQ} --data=${tag_root}/dev.fibo-quickstart.ttl --query=/publisher/lib/echo.sparql --results=NT > ${tag_root}/dev.fibo-quickstart.nt  
   ${JENA_ARQ} --data=${tag_root}/prod.fibo-quickstart.ttl --query=/publisher/lib/echo.sparql --results=NT > ${tag_root}/prod.fibo-quickstart.nt
   
@@ -675,7 +668,5 @@ __HERE__
   zip ${tag_root}/prod.fibo-quickstart.ttl.zip ${tag_root}/prod.fibo-quickstart.ttl
   zip ${tag_root}/dev.fibo-quickstart.nt.zip ${tag_root}/dev.fibo-quickstart.nt
   zip ${tag_root}/prod.fibo-quickstart.nt.zip ${tag_root}/prod.fibo-quickstart.nt
-
-}
 
 }
