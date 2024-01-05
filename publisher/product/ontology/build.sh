727ad48b1af4daeb0961a0254572fabbf38fab65
Pawel Garbacz <mereolog@users.noreply.github.com>
2024-01-05 11:56:34 +0100

    Update publisher/product/ontology/build.sh
    Co-authored-by: Przemysław Grądzki <przemek.gradzki@gmail.com>

--- publisher/product/ontology/build.sh
+++ publisher/product/ontology/build.sh
@@ -742,7 +742,7 @@ function createQuickVersions() {
     mv "${tag_root}/${quick_prod_spec}.ttl" ${tag_root}/${quick_prod_spec}-quickstart.ttl
 
   ${JENA_ARQ} --data=${tag_root}/${quick_dev_spec}-quickstart.ttl --query=/publisher/lib/echo.sparql --results=NT > ${tag_root}/${quick_dev_spec}-quickstart.nt
-  ${JENA_ARQ} --data=${tag_root}/${quick_prod_spec}-quickstart.ttl --query=/publisher/lib/echo.sparql --results=NT > ${tag_root}/${quick_prod_spec}-quickstart.nt
+  ${JENA_ARQ} --data=${tag_root}/${quick_prod_spec}.ttl --query=/publisher/lib/echo.sparql --results=NT > ${tag_root}/${quick_prod_spec}.nt
 
   zip ${tag_root}/${quick_dev_spec}-quickstart.ttl.zip ${tag_root}/${quick_dev_spec}-quickstart.ttl
   zip ${tag_root}/${quick_prod_spec}-quickstart.ttl.zip ${tag_root}/${quick_prod_spec}-quickstart.ttl
