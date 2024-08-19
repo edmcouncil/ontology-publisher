42168449292e2b64a4e2d7c8d2daa3c1f503cafd
mereolog <pawel.garbacz@makolab.com>
2024-08-19 16:18:33 +0200

    obsolete functions removed from _functions.sh file
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

--- publisher/lib/_functions.sh
+++ publisher/lib/_functions.sh
@@ -32,16 +32,6 @@ function isMacOSX() {
   test "$(uname -s)" == "Darwin"
 }
 
-function isDebian() {
-
-  test -f "/etc/debian_version"
-}
-
-function isWindowsWSL() {
-
-  test -f '/mnt/c/Windows/System32/cmd.exe'
-}
-
 function getWindowsEnvironmentVariable() {
 
   local variableName="$1"
@@ -152,11 +142,6 @@ function requireParameter() {
   exit 1
 }
 
-function isMacOSX() {
-
-  test "$(uname -s)" == "Darwin"
-}
-
 #
 # Create a temporary file with a given file extension.
 #
@@ -188,11 +173,6 @@ function mktempWithExtension() {
   fi
 
   return 0
-}
-
-function printfLog() {
-
-  printf -- "$*" >&2
 }
 
 function log() {
@@ -376,67 +356,6 @@ function errorNoSource() {
   return 1
 }
 
-function errorInCaller() {
-
-  if ((builder_no_error_prefix)) ; then
-    log "$*"
-    return 1
-  fi
-
-  if ((builder_running_inside_container)) ; then
-    echo "$(date "+%Y-%m-%d %H:%M:%S.%3N") ERROR: $@" >&2
-  else
-    local line="$*"
-    line="${line//[0m;/[0;208m}"
-    set -- $(caller 1)
-    printf "ERROR: in $(sourceLine $@): \\033[38;5;208m${line}\e[0m\n" >&2
-  fi
-
-  return 1
-}
-
-function errorInCallerOfCaller() {
-
-  if ((builder_no_error_prefix)) ; then
-    log "$*"
-    return 1
-  fi
-
-  if ((builder_running_inside_container)) ; then
-    echo "$(date "+%Y-%m-%d %H:%M:%S.%3N") ERROR: $@" >&2
-  else
-    local line="$*"
-    set -- $(caller 2)
-    printf "ERROR: in $(sourceLine $@): \\033[38;5;208m${line}\e[0m\n" >&2
-  fi
-
-  return 1
-}
-
-function printfError() {
-
-  if ((builder_no_error_prefix)) ; then
-    printfLog "$*"
-    return 1
-  fi
-
-  if ((builder_running_inside_container)) ; then
-    local -r timestamp="$(date "+%Y-%m-%d %H:%M:%S.%3N")"
-    local -r formatString="%s ERROR: $1"
-    shift
-    printf -- "${formatString}" "$@" >&2
-  else
-    local formatString="ERROR: %s: \\033[38;5;208m%s\e[0m $1"
-    shift
-    local line="$*"
-    # shellcheck disable=SC2046
-    set -- $(caller 0)
-    printf -- "${formatString}" "$(sourceLine "$@")" ${line} >&2
-  fi
-
-  return 1
-}
-
 #
 # Red is for error messages in light mode
 #
@@ -483,42 +402,6 @@ function bold() {
 function lightGrey() {
 
   printf "\e[37m$*\e[0m\n"
-}
-
-#
-# Log the given file with jq (pretty plus coloring) and if it's not valid JSON say so and dump file on the log
-#
-function logjson() {
-
-  local file="$1"
-
-  if [ ! -f "${file}" ] ; then
-    warning "${file} does not exist"
-    return 0
-  fi
-
-  if isJsonFile "${file}" ; then
-    ${JQ} . "${file}" >&2 # pipelog strips ANSI colors
-    return 0
-  fi
-
-  log "${file} is not valid JSON:"
-  cat "${file}" | pipelog
-  log "-----"
-
-  return 1
-}
-
-#
-# Return true (0) if the given file is a JSON file.
-#
-function isJsonFile() {
-
-  local file="$1"
-
-  [ -f "${file}" ] || return 1
-
-  ${JQ} . "$1" >/dev/null 2>&1
 }
 
 function sourceFile() {
@@ -954,98 +837,6 @@ function initJiraVars() {
   return 0
 }
 
-function stripQuotes() {
-
-  local temp="$*"
-
-  case "${temp}" in
-    *[!\ ]*)
-      temp="${temp%\"}"
-      temp="${temp#\"}"
-      echo -n "$temp"
-      ;;
-    *)
-      echo -n ""
-      ;;
-  esac
-}
-
-function stripQuotes_test_0003_001() {
-
-  test "$(stripQuotes "\" \"")" == "" || return $?
-  test "$(stripQuotes "\"abc\"")" == "abc" || return $?
-  test "$(stripQuotes "\"abc \"def\"\"")" == "abc \"def\"" || return $?
-
-  return 0
-}
-
-#
-# See https://tex.stackexchange.com/a/34586
-#
-function escapeLaTex() {
-
-  local line="$*"
-
-  line="${line//\\/\\\\textbackslash }"
-
-  line="${line//%/\\%}"
-  line="${line//\$/\\$}"
-  line="${line//#/\\#}"
-  line="${line//_/\\_}"
-  line="${line//\{/\\\{}"
-  line="${line//\}/\\\}}"
-  line="${line//&/{\\&\}}"
-
-  line="${line//\~/\\\\textasciitilde }"
-  line="${line//^/\\\\textasciicircum }"
-
-  echo -n "${line}"
-}
-
-function escapeLaTex_test_002_0001() {
-
-  local -r result="$(escapeLaTex "whatever_dude\what %is% #metoo in {this} ~day & ^age")"
-
-  echo "[${result}]"
-
-  test "${result}" == "whatever\_dude\\textbackslash what \%is\% \#metoo in \{this\} \\textasciitilde day {\&} \\textasciicircum age"
-}
-#escapeLaTex_test_002_0001
-#exit $?
-
-function escapeLaTexLabel() {
-
-  local line="$*"
-
-  echo -n "${line//[_&%\$#\{\}~^]/-}"
-}
-
-function escapeLaTexLabel_test_001_0002() {
-
-  local -r result="$(escapeLaTexLabel "abc_def&ghi%klm$nop#qrs{tuv}xyz~!^")"
-
-  test "${result}" == "abc-def-ghi-klm-qrs-tuv-xyz-!-"
-}
-
-function escapeAndDetokenizeLaTex() {
-
-  local line="$*"
-
-  #
-  # Don't use printf here
-  #
-  echo -n "\detokenize{"
-  escapeLaTex "${line}"
-  echo -n "}"
-}
-
-function escapeAndDetokenizeLaTex_test_001_0001() {
-
-  local -r input="Rights on the lender to protect them against loss. furthe rNtoes: Logically, considering the two parties, they both have protecxtion mechanisms. so while the lender has protecxtion mechanisms through mortgage insurance, and the consumer has protextion mechanisms such as good faith estimates. also the agencies (see Consumer Protection Agency), an instance of which is the CFPB in the US (just set up). Lender rights are: - expressed in the Contract Consumer protection develops becaues the contract is written by the potential Lender. So the rights are introcued to rectify the imbalance between the two parties. Same goes for insurance. consumer protection laws (governe dby the relevant consumer protection agency. So the lender protexts itself as it writes th contract AND does the things it needs to do to protext itself, but on the approval process, and with later instruments such as insurance. Interestingly., it is the Borrower who pays for this by paying for credit reports etc. So the borrower protects itself by other mechanisms. Caveat emptor - displaced by regulation (the buyer is protected by regulation). Uberimae Fidae - in the utmost good faith. Mortgage Insurance is an additional means of mitigating the risk, that the lended may have., so if the information assessed is not accurate, or if the borrower's situation changes for the worse. then the risk rating may go down. So the Mortgage Insurance is a further strategy which mitigates any shortfall in the Lender Righrs that you may have - ie someone guarantees. In the US you can also avoid that by having paid a deposit. PIMI: Principal, Interst and Morgage Insurance. So the Borrower pays towards the MI, esxcept if they have paid a given amount as deposit. there are 2 types of MI: 1. protects the lender in the event of borrower degault 2. Insurance for \"Incapacity to pay the mortage\" (these can be bought off the shelf - can combine health, unemployment etc.). - this is the Borrower mitigating their own risk. Prevents foreclosure. Similar to general sickness etc. Where the lender charges for MI, the cost is passed onto the Borrower. e.g. if there is a % valuation (e.g. 70% in Aus, 80% in US for example) then no insurance is required."
-
-  echo "$(escapeAndDetokenizeLaTex "${input}")"
-}
-
 #
 # Return true if running inside a docker container.
 # See https://stackoverflow.com/a/41559867/1110667
@@ -1070,57 +861,6 @@ function getUris() {
     cat "${1}" 2>/dev/null | xml sel -N _=urn:oasis:names:tc:entity:xmlns:xml:catalog -t -v "/_:catalog/_:uri[@name=\"${name}\"]/@uri" -nl
    # 2>/dev/null || echo "${name}"
   done
-}
-
-function getDevOntologiesInRDFXMLFormatInCurrentDirectory() {
-
-  ls -1 *.rdf | \
-    ${GREP} -v ont-policy.rdf | \
-    ${GREP} -v ".*/etc.*" | \
-    ${GREP} -v ".*About.*" | \
-    ${GREP} -v ".*temp.*" | \
-    ${GREP} -v "dev.*" | \
-    ${GREP} -v "prod.*" | \
-    ${GREP} -v "All.*" | \
-    ${GREP} -v ".*Metadata.*" | \
-    ${GREP} -v ".*Load.*" | \
-    ${GREP} -v ".*\.rdfX" | \
-    ${GREP} -v "\.rdf\.orig"
-}
-
-function getDevOntologiesInTurtleFormatInCurrentDirectory() {
-
-  ls -1 *.ttl | \
-    ${GREP} -v ont-policy.ttl | \
-    ${GREP} -v ".*/etc.*" | \
-    ${GREP} -v ".*About.*" | \
-    ${GREP} -v ".*temp.*" | \
-    ${GREP} -v "dev.*" | \
-    ${GREP} -v "prod.*" | \
-    ${GREP} -v "All.*" | \
-    ${GREP} -v ".*Metadata.*" | \
-    ${GREP} -v ".*Load.*" | \
-    ${GREP} -v ".*\.rdfX" | \
-    ${GREP} -v "\.rdf\.orig"
-}
-
-#
-# Return all the .rdf files that go into "prod"
-#
-function getProdOntologies() {
-
-  requireValue ontology_product_tag_root || return $?
-
-  ${GREP} -rl 'utl-av[:;.]Release' "${ontology_product_tag_root}" | \
-    ${GREP} -F ".rdf" | \
-    ${GREP} -v ont-policy.rdf | \
-    ${GREP} -v "*About*" | \
-    ${GREP} -v "*Metadata*" | \
-    ${GREP} -v "*Load*" | \
-    ${GREP} -v "*.rdfX" | \
-    ${GREP} -v ".rdf.orig" | \
-    ${GREP} -v '*About*' | \
-    ${GREP} -v '/etc/'
 }
 
 #
@@ -1152,16 +892,6 @@ function getOntologyVersionIRI() {
 
   xml -q c14n 2>/dev/null | xml sel \
 		-t -v '/rdf:RDF/owl:Ontology/owl:versionIRI/@rdf:resource' -nl 2>/dev/null \
-			| sed -r '/^\W*$/d' | head -n 1
-}
-
-#
-# Returns ontology prefix
-#
-function getOntologyPrefix() {
-
-  xml -q c14n 2>/dev/null | xml sel \
-		-t -v '/rdf:RDF/owl:Ontology/sm:fileAbbreviation' -nl 2>/dev/null \
 			| sed -r '/^\W*$/d' | head -n 1
 }
 
